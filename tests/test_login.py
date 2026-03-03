5e618497e288f4e5d9fd9f55e430629a06c283b9
surya22tsbs-hub <surya22tsbs@gmail.com>
2026-03-03 13:15:58 +0530

    Testcases modified

--- tests/test_login.py
+++ tests/test_login.py
@@ -8,21 +8,21 @@ def load_data():
 #Testcases
 
 #1. Load the login page.
-def test_login(home_page_login):
-    data = load_data()
+@pytest.mark.parametrize("data",load_data())
+def test_login(home_page_login, data):
     home_page_login.navigate_to(data["url"])
 
 #2. Interact with the interface to change to the username/password version of the login page.
 #3. Input the username and password into their corresponding fields.
 #4. Click the login button to proceed to the logged-in state.
-def test_load_login(home_page_login):
-    data = load_data()
+@pytest.mark.parametrize("data",load_data())
+def test_load_login(home_page_login, data):
     home_page_login.navigate_to(data["url"])
     home_page_login.login(data["username"], data["password"])  #Replace with the user credentials from login.json file
 
 #5. Check that there is a loaded user number on the dashboard (the content needs to be present but could be a changing value)
-def test_dashboard(home_page_login):
-    data = load_data()
+@pytest.mark.parametrize("data",load_data())
+def test_dashboard(home_page_login, data):
     home_page_login.navigate_to(data["url"])
     home_page_login.login(data["username"], data["password"])
     user = data["username"][:5]
